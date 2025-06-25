21bd4da97b7be1583b9a189f277473863aec8f89
kapil_mighani <158256649+kapilmighani@users.noreply.github.com>
2025-06-25 19:12:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,15 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
