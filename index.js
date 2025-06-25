4228e9c4d489b4875f0c10de7c3d7312d0826352
kapil_mighani <158256649+kapilmighani@users.noreply.github.com>
2025-06-25 08:30:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ mongoose
 import cors from "cors";
 
 app.use(cors({
-  origin: "https://stayfinder-frontend.vercel.app/",
+  origin: "https://stayfinder-frondend.vercel.app",
   credentials: true,
 }));
 
